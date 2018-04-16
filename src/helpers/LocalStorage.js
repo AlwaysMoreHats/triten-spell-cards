15c4fd5121e6bdcbac79161696eb72ab0e42bc68
Stephen Smith <sns1215@gmail.com>
2018-04-15 23:13:14 -0400

    Updates

--- src/helpers/LocalStorage.js
+++
@@ -1,6 +0,0 @@
-const SPELL_DATA = 'spellData'
-
-const saveSpellData = jsonString => localStorage.setItem(SPELL_DATA, jsonString)
-const getSpellData = () => localStorage.getItem(SPELL_DATA)
-
-export { getSpellData, saveSpellData }
